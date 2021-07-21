e33df4acf0defbe959cfe80e0185fe38e604644e
gabyiy <gabyiy2000@hotmail.com>
2021-07-21 20:01:23 +0000

    sec

--- src/js/component/traficLighth.js
+++ src/js/component/traficLighth.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./traficLight.css";
 
 export function TraficLight() {
+	const [color, setColor] = useState("next");
 	return (
-		<div>
-			<h1>Trafic light</h1>
+		<div className="center">
+			<h1
+				className={`red divAll  ${color === "red" ? "shadow" : null}`}
+				onClick={() => setColor("red")}></h1>
+			<h1
+				className={`green divAll  ${
+					color === "green" ? "shadow" : null
+				}`}
+				onClick={() => setColor("green")}></h1>
+			<h1
+				className={`yellow divAll ${
+					color === "yellow" ? "shadow" : null
+				}`}
+				onClick={() => setColor("yellow")}></h1>
 		</div>
 	);
 }
